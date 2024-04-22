ff59de075c5b21b3264e1d429cc3e71a363628ae
maxwynne <76392789+maxwynne@users.noreply.github.com>
2024-04-22 16:56:07 +0100

    Update staticassert1.cpp

--- staticassert1.cpp
+++ staticassert1.cpp
@@ -1,12 +1,10 @@
 // static assert mechanism can be used for anything expressed in terms of constant expressions
 
-constexpr double C = 299792.458;
+constexpr double C = 299792.458; // km/s
 
 void f(double speed)
 {
-     const double local_max = 160.0/(60*60)
-     static_assert(speed<C,"can't go that fast");
-     static_assert(local_max<C,"can't go that fast");
-
-     // ...
+     const double local_max = 160.0/(60*60); // 160 km/h == 160.0/(60*60) km/s
+     static_assert(speed<C,"can't go that fast"); // speed must be a constant
+     static_assert(local_max<C,"can't go that fast"); // OK
 }
