fa51aea43bacaf4a3d0c672d9fa66c2d227f149c
maxwynne <76392789+maxwynne@users.noreply.github.com>
2024-04-22 18:48:05 +0100

    Update staticassert3.cpp

--- staticassert3.cpp
+++ staticassert3.cpp
@@ -10,6 +10,6 @@ vector<int> vector1{1, 2, 3, 4, 5};
 cout << "vector1 = ";
 
 // ranged loop
-for (const int& i : vector1) { // const int - returns a constant int
+for (const int& i : vector1) { // const int - returns a constant int, a variable that does not change value through lifetime
   cout << i << " "; 
 }
